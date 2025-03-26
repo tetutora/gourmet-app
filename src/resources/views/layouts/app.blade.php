4f49ad1fe0d950e9b1f3082209c98721e2b15b94
kitano <beiyelinai01@gmail.com>
2025-03-26 16:15:35 +0900

    Fotifyインストール

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/layouts/app.css' }}">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css' }}">
+    <title>gourmet-app</title>
+    @yield('css')
+</head>
+<body>
+    <header class="heder">
+        <div class="header__inner">
+            
+        </div>
+    </header>
+    <ul class="header-nav">
+        @if (Auth::check())
+
+    </ul>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [AuthController::class, 'showRegister']);
